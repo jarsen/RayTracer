2f14f7c1eff290092ea8ac2bfab0c02cecb16c18
Jason Larsen <jason.larsen@gmail.com>
2015-10-24 17:30:58 -0600

    Initial Commit

---
+++ RayTracer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RayTracer
+//
+//  Created by Jason Larsen on 10/24/15.
+//  Copyright © 2015 Heartbit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RayTracer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RayTracer
+//
+//  Created by Jason Larsen on 10/24/15.
+//  Copyright © 2015 Heartbit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
