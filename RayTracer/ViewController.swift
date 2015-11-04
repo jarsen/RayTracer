a818cede75dbbbee014621c6f81b162f4290c4e2
Jason Larsen <jason.larsen@gmail.com>
2015-11-04 15:54:06 -0700

    adjust scene1

--- RayTracer/ViewController.swift
+++ RayTracer/ViewController.swift
@@ -27,10 +27,10 @@ class ViewController: NSViewController {
         scene.lookFrom = Point(0, 0, 10)
         scene.backgroundColor = NSColor(calibratedRed: 0.7, green: 0.7, blue: 0.7, alpha: 1)
         let sphere1 = Sphere(center: Point(-2, 0, 0), radius: 2, material: .Diffuse(color: Color(1, 0, 0), specularHighlight: Color(1, 1, 1), phongConstant: 16))
-        let sphere2 = Sphere(center: Point(1, 0, -2), radius: 2, material: .Diffuse(color: Color(0, 1, 0), specularHighlight: Color(1, 1, 1), phongConstant: 16))
-        let sphere3 = Sphere(center: Point(0.5, 2.5, 0), radius: 1, material: .Diffuse(color: Color(1, 1, 1), specularHighlight: Color(1, 1, 1), phongConstant: 16))
-        let triangle1 = Polygon(points: [Point(-5, -3, 5), Point(-5, -3, -5), Point(5, -3, -5), Point(5, -3, 5)], material: .Diffuse(color: Color(1, 1, 1), specularHighlight: Color(1, 1, 1), phongConstant: 16))
-        scene.objects = [sphere1, sphere2, sphere3, triangle1]
+        let sphere2 = Sphere(center: Point(1, 0, -2), radius: 1.5, material: .Diffuse(color: Color(0, 1, 0), specularHighlight: Color(1, 1, 1), phongConstant: 16))
+        let sphere3 = Sphere(center: Point(0.5, 2.5, 0), radius: 0.5, material: .Diffuse(color: Color(1, 1, 1), specularHighlight: Color(1, 1, 1), phongConstant: 16))
+        let floor = Polygon(points: [Point(-5, -3, 5), Point(-5, -3, -10), Point(5, -3, -10), Point(5, -3, 5)], material: .Diffuse(color: Color(1, 1, 1), specularHighlight: Color(1, 1, 1), phongConstant: 16))
+        scene.objects = [sphere1, sphere2, sphere3, floor]
         scene.ambientLight = Color(0.3, 0.3, 0.3)
         var lightSource = DirectionalLightSource()
         lightSource.direction = Vector(0, 10, 4).normalized()
