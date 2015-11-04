c4e9aed63e214ce3e9960801c3f3235123419849
Jason Larsen <jason.larsen@gmail.com>
2015-11-03 20:56:55 -0700

    make triangle into floor for `scene1`

--- RayTracer/ViewController.swift
+++ RayTracer/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: NSViewController {
         let sphere1 = Sphere(center: Point(-2, 0, 0), radius: 2, material: .Diffuse(color: Color(1, 0, 0), specularHighlight: Color(1, 1, 1), phongConstant: 16))
         let sphere2 = Sphere(center: Point(1, 0, -2), radius: 2, material: .Diffuse(color: Color(0, 1, 0), specularHighlight: Color(1, 1, 1), phongConstant: 16))
         let sphere3 = Sphere(center: Point(0.5, 2.5, 0), radius: 1, material: .Diffuse(color: Color(1, 1, 1), specularHighlight: Color(1, 1, 1), phongConstant: 16))
-        let triangle1 = Polygon(points: [Point(-1,-1,0), Point(0,1,-0.1), Point(1,-1,0)], material: .Diffuse(color: Color(1, 1, 1), specularHighlight: Color(1, 1, 1), phongConstant: 16))
+        let triangle1 = Polygon(points: [Point(-5, -3, 5), Point(-5, -3, -5), Point(5, -3, -5), Point(5, -3, 5)], material: .Diffuse(color: Color(1, 1, 1), specularHighlight: Color(1, 1, 1), phongConstant: 16))
         scene.objects = [sphere1, sphere2, sphere3, triangle1]
         scene.ambientLight = Color(0.3, 0.3, 0.3)
         var lightSource = DirectionalLightSource()
